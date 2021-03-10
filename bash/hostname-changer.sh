39e41ae647f37b084cd82fad0afef878bef328b5
mnprtbhullar <Manpreetkaurbhullar01@gmailcom>
2021-03-10 02:03:25 -0500

    lab 2 fixed

--- bash/hostname-changer.sh
+++ bash/hostname-changer.sh
@@ -20,3 +20,17 @@
 # If that hostname is not the current hostname, change it using the hostnamectl command and
 #     tell the user you changed the current hostname and they should reboot to make sure the new name takes full effect
 #e.g. hostnamectl set-hostname $newname
+
+
+hostlocation=$(cat /etc/hostname) #curent hostname
+echo "Name of current host is  $hostlocation" # telling user hostname
+echo "Type student number - "
+read typestudentnum  # getting input from user
+hostnamectl set-hostname pc$typestudentnum
+replacehost=$(hostname)
+echo "New hostname is $replacehost"
+echo "Changed old hostanme to the new hostnmae using sed command"
+sudo sed -i "s/$hostlocation/$replacehost/g" /etc/hosts # replacing hostanem
+echo "Hostname changed."
+read -s -n 1 -p "System needs to reboot to make sure the new hostname takes full effect. Please press any key."
+sudo reboot
